f365d2b2f3f101f7e7aa1224c8be4009dbfe3f85
Harper Jiang <harperjiang@msn.com>
2017-02-22 14:17:19 -0600

    Fix a bug in parameter saving

--- project/run.py
+++ project/run.py
@@ -36,21 +36,21 @@ bo = []
 
 for i in range(layer):
     # forget gate
-    Wf.append(edf.Param(edf.xavier((2*hidden_dim, hidden_dim))))
+    Wf.append(edf.Param(edf.xavier((2 * hidden_dim, hidden_dim))))
     bf.append(edf.Param(np.zeros((hidden_dim))))
     # input gate
-    Wi.append(edf.Param(edf.xavier((2*hidden_dim, hidden_dim))))
+    Wi.append(edf.Param(edf.xavier((2 * hidden_dim, hidden_dim))))
     bi.append(edf.Param(np.zeros((hidden_dim))))
     # carry cell
-    Wc.append(edf.Param(edf.xavier((2*hidden_dim, hidden_dim))))
+    Wc.append(edf.Param(edf.xavier((2 * hidden_dim, hidden_dim))))
     bc.append(edf.Param(np.zeros((hidden_dim))))
     # output cell
-    Wo.append(edf.Param(edf.xavier((2*hidden_dim, hidden_dim))))
+    Wo.append(edf.Param(edf.xavier((2 * hidden_dim, hidden_dim))))
     bo.append(edf.Param(np.zeros((hidden_dim))))
 
 V = edf.Param(edf.xavier((hidden_dim, n_vocab)))
 
-parameters.extend([C2V, Wf, bf, Wi, bi, Wc, bc, Wo, bo, V])
+parameters.extend([C2V, V, Wf, bf, Wi, bi, Wc, bc, Wo, bo])
 
 
 # load the trained model if exist
@@ -88,16 +88,16 @@ def BuildModel():
         c[0][i] = edf.Value(np.zeros((B, hidden_dim)))
     score = []
 
-    for t in range(T-1):
+    for t in range(T - 1):
 
-        wordvec = edf.Embed(edf.Value(inp.value[:,t]), C2V)
+        wordvec = edf.Embed(edf.Value(inp.value[:, t]), C2V)
         xt = edf.Reshape(wordvec, [-1, hidden_dim])
 
         for i in range(layer):
-            h[t+1][i], c[t+1][i] = LSTMCell(xt, h[t][i], c[t][i], i)
-            xt = h[t+1][i]
+            h[t + 1][i], c[t + 1][i] = LSTMCell(xt, h[t][i], c[t][i], i)
+            xt = h[t + 1][i]
         p = edf.SoftMax(edf.VDot(xt, V))
-        logloss = edf.Reshape(edf.LogLoss(edf.Aref(p, edf.Value(inp.value[:,t+1]))), (B, 1))
+        logloss = edf.Reshape(edf.LogLoss(edf.Aref(p, edf.Value(inp.value[:, t + 1]))), (B, 1))
 
         if t == 0:
             loss = logloss
@@ -106,8 +106,8 @@ def BuildModel():
 
         score.append(p)
 
-    masks = np.zeros((B, T-1), dtype = np.int32)
-    masks[inp.value[:,1:] != 0] = 1
+    masks = np.zeros((B, T - 1), dtype=np.int32)
+    masks[inp.value[:, 1:] != 0] = 1
     loss = edf.MeanwithMask(loss, edf.Value(masks))
 
     return loss, score
@@ -116,23 +116,23 @@ def BuildModel():
 def CalPerp(score):
 
     prob = [p.value for p in score]
-    prob = np.transpose(np.stack(prob, axis = 0),(1,0,2))
+    prob = np.transpose(np.stack(prob, axis=0), (1, 0, 2))
 
     B = prob.shape[0]
     T = prob.shape[1]
     V = prob.shape[2]
 
     masks = np.zeros((B, T), dtype=np.int32)
-    masks[inp.value[:,1:] != 0] = 1
+    masks[inp.value[:, 1:] != 0] = 1
 
     prob = prob.reshape(-1)
-    idx = np.int32(inp.value[:,1:].reshape(-1))
+    idx = np.int32(inp.value[:, 1:].reshape(-1))
     outer_dim = len(idx)
-    inner_dim = len(prob)/outer_dim
-    pick = np.int32(np.array(range(outer_dim))*inner_dim + idx)
+    inner_dim = len(prob) / outer_dim
+    pick = np.int32(np.array(range(outer_dim)) * inner_dim + idx)
     prob = prob[pick].reshape(B, T)
 
-    return -np.sum(np.log(prob[np.nonzero(prob*masks)]))
+    return -np.sum(np.log(prob[np.nonzero(prob * masks)]))
 
 def Predict(max_step, prefix):
 
@@ -158,8 +158,8 @@ def Predict(max_step, prefix):
         wordvec = edf.Embed(pred, C2V)
         xt = edf.Reshape(wordvec, [-1, hidden_dim])
         for i in range(layer):
-            h[t+1][i],c[t+1][i] = LSTMCell(xt, h[t][i], c[t][i], i)
-            xt = h[t+1][i]
+            h[t + 1][i], c[t + 1][i] = LSTMCell(xt, h[t][i], c[t][i], i)
+            xt = h[t + 1][i]
         p = edf.SoftMax(edf.VDot(xt, V))
         pred = edf.ArgMax(p)
 
@@ -169,7 +169,7 @@ def Predict(max_step, prefix):
     stop_idx = utils.to_index('}')
 
     if stop_idx in idx:
-        return idx[0:idx.index(stop_idx)+1]
+        return idx[0:idx.index(stop_idx) + 1]
     else:
         return idx
 
@@ -178,7 +178,7 @@ def Eval(data, cnt):
     perp = 0.
     avg_loss = 0.
     test_batches = range(0, len(data), batch)
-    test_minbatches = [data[idx:idx+batch] for idx in test_batches]
+    test_minbatches = [data[idx:idx + batch] for idx in test_batches]
 
     for minbatch in test_minbatches:
 
@@ -189,7 +189,7 @@ def Eval(data, cnt):
         avg_loss += loss.value
         perp += CalPerp(score)
 
-    perp = np.exp(perp/cnt)
+    perp = np.exp(perp / cnt)
     avg_loss /= len(test_batches)
     return perp, avg_loss
 
@@ -197,7 +197,7 @@ def Eval(data, cnt):
 ############################################### training loop #####################################################
 
 batches = range(0, len(train_data), batch)
-minbatches = [train_data[idx:idx+batch] for idx in batches]
+minbatches = [train_data[idx:idx + batch] for idx in batches]
 
 epoch = 30
 
@@ -214,7 +214,7 @@ print (utils.to_string(generation))
 for ep in range(epoch):
 
     perm = np.random.permutation(len(minbatches)).tolist()
-    stime=time()
+    stime = time()
 
     for k in range(len(minbatches)):
 
@@ -227,7 +227,7 @@ for ep in range(epoch):
         edf.GradClip(10)
         edf.SGD(eta)
 
-    duration = (time() - stime)/60.
+    duration = (time() - stime) / 60.
 
     perp, loss = Eval(valid_data, vacnt)
     print("Epoch %d: Perplexity: %0.5f Avg loss = %0.5f [%.3f mins]" % (ep, perp, loss, duration))
@@ -245,8 +245,11 @@ for ep in range(epoch):
         f = open(model, 'wb')
         p_value = []
         for p in parameters:
-            for i in range(layer):
-                p_value.append(p[i].value)
+            if isinstance(p, list):
+                for i in range(layer):
+                    p_value.append(p[i].value)
+            else:
+                p_value.append(p.value)
         pickle.dump(p_value, f)
 
     else:
@@ -257,7 +260,11 @@ for ep in range(epoch):
         with open(model, 'rb') as f:
             p_value = pickle.load(f)
             idx = 0
-            for p in p_value:
-                for i in range(layer):
-                    parameters[idx * plength + i].value = p
-                idx += 1
+            for p in parameters:
+                if isinstance(p, list):
+                    for i in range(layer):
+                        p[i].value = p_value[idx]
+                        idx += 1
+                else:
+                    p.value = p_value[idx]
+                    idx += 1
